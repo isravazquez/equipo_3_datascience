0354ebdff77e919a09a14ed90ddb2acbbdbe2f65
hugomi <hugomtzib@gmail.com>
2021-07-13 23:06:40 -0500

    Nuevo archivo con todos los postwork hasta el 7

--- Equipo3_Postwork_1-7.R
+++ Equipo3_Postwork_1-7.R
@@ -7,12 +7,16 @@
 # Vazquez Bernal Jaime Israel, israfullshot@gmail.com
 
 # Índice
-# Postwork 1, línea 17
-# Postwork 2, línea 68
-# Postwork 3, línea 126
-# Postwork 4, línea 207
+# Postwork 1, línea 21
+# Postwork 2, línea 83
+# Postwork 3, línea 154
+# Postwork 4, línea 235
+# Postwork 5, línea 345
+# Postwork 6, línea 413
+# Postwork 7, línea 491
 
-# NOTA: Cambiar dirección donde se descargarán y leerán los archivos CSV (en la línea 85).
+## NOTA: Cambiar dirección donde se descargarán y leerán los archivos CSV (en la línea 100).
+## Recordar que debe ser una carpeta vacía para que sólo se lean los archivos descargados.
 
 # POSTWORK 1
 
@@ -60,6 +64,19 @@ sum(table_scores)
 (marginal_prob_visitor <- c(marginal_prob_away, sum(marginal_prob_home)))
 # Se termina de crear la tabla, al pegar los datos anteriores.
 (summary_table_probs <- rbind(summary_table_probs, marginal_prob_visitor))
+# Tabla resumen de la probabilidad conjunta y las probabilidades marginales.
+(final_summary <- as.data.frame(round(summary_table_probs, 4)))
+
+# install.packages("reactable")
+library(reactable)
+(tabla_final <- reactable(final_summary, theme = reactableTheme(
+  borderColor = "#dfe2e5",
+  stripedColor = "#f6f8fa",
+  highlightColor = "#f0f5f9",
+  cellPadding = "8px 12px",
+  style = list(fontFamily = "-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif"),
+  searchInputStyle = list(width = "100%")
+)))
 
 
 
@@ -80,13 +97,16 @@ url1819 <- "https://www.football-data.co.uk/mmz4281/1819/SP1.csv"
 url1920 <- "https://www.football-data.co.uk/mmz4281/1920/SP1.csv"
 
 # NOTA: Modificar la dirección según el caso donde se deseen guardar y leer los archivos.
-setwd("/home/hugomi/Documents/BEDU/Modulo2/Postwork3/DataPW")
+# Recordar que debe ser una carpeta vacía, unicamente para estos archivos.
+path <- "/home/hugomi/Documents/BEDU/Modulo2/Postwork3/Archivos"
+setwd(path)
 
+# Se descargan los archivos en la carpeta indicada.
 download.file(url = url1920, destfile = "D-1920.csv", mode = "wb")
 download.file(url = url1819, destfile = "D-1819.csv", mode = "wb")
 download.file(url = url1718, destfile = "D-1718.csv", mode = "wb")
 
-# Importa los archivos descargados a R
+# Importa los archivos descargados a R, en una lista de listas.
 raw_data <- lapply(dir(), read.csv)
 
 # Revisa la estructura de de los data frames al usar las funciones: str, head, View y summary.
@@ -118,6 +138,16 @@ data_mod2 <- lapply(data_mod1, mutate, Date = as.Date(Date, "%d/%m/%Y"))
 data_cleaned <- do.call(rbind, data_mod2)
 head(data_cleaned)
 dim(data_cleaned)
+
+# Tabla muestra de los datos limpios.
+(tabla_limpia_muestra <- reactable(head(data_cleaned, 10), theme = reactableTheme(
+  borderColor = "#dfe2e5",
+  stripedColor = "#f6f8fa",
+  highlightColor = "#f0f5f9",
+  cellPadding = "8px 12px",
+  style = list(fontFamily = "-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif"),
+  searchInputStyle = list(width = "100%")
+)))
 
 
 
@@ -239,7 +269,8 @@ quotients.proba(joint_proba)
 # Mediante un procedimiento de boostrap, obtén más cocientes similares a los obtenidos en la tabla
 # del punto anterior. Esto para tener una idea de las distribuciones de la cual vienen los cocientes
 # en la tabla anterior. Menciona en cuáles casos le parece razonable suponer que los cocientes de la
-# tabla en el punto 1, son iguales a 1 (en tal caso tendríamos independencia de las variables aleatorias X y Y).
+# tabla en el punto 1, son iguales a 1 (en tal caso tendríamos independencia de las variables
+# aleatorias X y Y).
 
 # Primero filtraremos sólo las columnas necesarias del data frame de los datos de goles que resultó
 # de la limpieza.
@@ -249,7 +280,7 @@ goals <- select(data_cleaned, FTHG, FTAG)
 # Procedimiento boostrap
 
 # Se generan muestreos de los datos de goles un cierto némero de veces o repeticiones.
-repetitions <- 100
+repetitions <- 1000
 goals_samples <- list()
 for(i in 1:repetitions){
   goals_samples[[i]] <- sample_n(goals,dim(goals)[1],replace = T)
@@ -261,7 +292,8 @@ for(i in 1:repetitions){
 # Ahora, se crean las tablas de frecuencia para cada muestra.
 freq_absolute <- lapply(goals_samples, table)
 
-# Esta función convierte la tabla de frecuencias absolutas (de arriba) en una tabla de frecuencias relativas.
+# Esta función convierte la tabla de frecuencias absolutas (de arriba) en una tabla de
+# frecuencias relativas.
 freq.relative <- function(tabla){
   tabla <- tabla/sum(tabla)
 }
@@ -307,4 +339,182 @@ if (p_value < 0.05){
 hist(mean_samples, col = "gold", border = "black",
      main = "Histograma de medias",
      xlab = "Medias", ylab = "Frecuencias")
+
+
+
+# POSTWORK 5
+
+# 1. A partir del conjunto de datos de soccer de la liga española de las temporadas 2017/2018, 2018/2019 y
+# 2019/2020, crea el data frame SmallData, que contenga las columnas date, home.team, home.score,
+# away.team y away.score; esto lo puede hacer con ayuda de la función select del paquete dplyr.
+# Luego establece un directorio de trabajo y con ayuda de la función write.csv guarda el data frame como un
+# archivo csv con nombre soccer.csv. Puedes colocar como argumento row.names = FALSE en write.csv.
+
+# Se lelva a cabo la selección de las columnas requeridas para el análisis.
+SmallData <- select(data_cleaned, Date:FTAG)
+# Se renombran las columnas con los nombres requeridos para las siguientes funciones.
+SmallData <- rename(SmallData, date = Date, home.team = HomeTeam, away.team = AwayTeam, home.score = FTHG, away.score = FTAG)
+
+# Se establece la  ruta de trabajo.
+setwd("/home/hugomi/Documents/BEDU/Modulo2/Postwork5")
+
+# Se escriben los datos en el archivo soccer.csv
+write.csv(SmallData, "soccer.csv", quote = T, row.names = F)
+
+# 2. Con la función create.fbRanks.dataframes del paquete fbRanks importe el archivo soccer.csv a R y al mismo
+# tiempo asignelo a una variable llamada listasoccer. Se creará una lista con los elementos scores y teams
+# que son data frames listos para la función rank.teams. Asigna estos data frames a variables llamadas
+# anotaciones y equipos.
+
+# install.packages("fbRanks")
+library(fbRanks)
+
+# Se usa la función create.fbRanks.dataframes() que tiene como argumento el archivo de datos anterior.
+listasoccer <- create.fbRanks.dataframes(scores.file = "soccer.csv")
+# Esto crea una lista de listas que contiene entre otras cosas, los nombres de los equipos y los datos de
+# las anotaciones.
+
+# Dichos datos se alamcenan en las siguientes variables.
+anotaciones <- listasoccer[[1]]
+equipos <- listasoccer[[4]]
+
+# 3. Con ayuda de la función unique crea un vector de fechas (fecha) que no se repitan y que correspondan
+# a las fechas en las que se jugaron partidos. Crea una variable llamada n que contenga el número de fechas
+# diferentes. Posteriormente, con la función rank.teams y usando como argumentos los data frames anotaciones
+# y equipos, crea un ranking de equipos usando únicamente datos desde la fecha inicial y hasta la penúltima
+# fecha en la que se jugaron partidos, estas fechas las deberá especificar en max.date y min.date. Guarda
+# los resultados con el nombre ranking.
+
+# Se crea la lista de fechas de partidos jugados
+fecha <- unique(anotaciones$date)
+# Se ordenan las fechas.
+fecha <- sort(fecha)
+# Se cuenta el número total de fechas.
+n <- length(fecha)
+
+# Se obtiene la fecha inicial y la penúltima, requeridas para la siguiente función.
+min_fecha = fecha[1]
+max_fecha = fecha[n-1]
+
+# Se crea el objeto ranking que contiene un análisis de los partidos a aprtir de los datos extraidos anteriormente.
+ranking <- rank.teams(scores = anotaciones, teams = equipos,  family="poisson", fun="glm", max.date = max_fecha, min.date = min_fecha)
+
+# 4. Finalmente estima las probabilidades de los eventos, el equipo de casa gana, el equipo visitante gana
+# o el resultado es un empate para los partidos que se jugaron en la última fecha del vector de fechas fecha.
+# Esto lo puedes hacer con ayuda de la función predict y usando como argumentos ranking y fecha[n] que deberá
+# especificar en date.
+
+# Con el análisis anterior, se usa como argumento de la función predict, para determinar las probabilidades de
+# los eventos mencionados para la última fecha (partido), fecha[n].
+predict(ranking, date = fecha[n])
+
+
+
+# POSTWORK 6
+
+# Importa el conjunto de datos match.data.csv a R y realiza lo siguiente:
+
+# Se hace la lectura de los datos.
+data_match <- read.csv("https://raw.githubusercontent.com/beduExpert/Programacion-R-Santander-2021/main/Sesion-06/Postwork/match.data.csv")
+# Se da el formato correcto a los datos de la columna fecha.
+data_match <- mutate(data_match, date = as.Date(date, "%Y-%m-%d"))
+
+# 1. Agrega una nueva columna sumagoles que contenga la suma de goles por partido.
+
+# Se suman los goles del equipo local y el visitante pro partido (renglón).
+data_match$sumagoles <- data_match$home.score + data_match$away.score
+
+# 2. Obtén el promedio por mes de la suma de goles.
+
+# Se realiza una agrupación por mes y año para obtener el promedio por mes.
+promedio_por_mes <- data_match %>%
+  mutate(month = format(date, "%m"), year = format(date, "%Y")) %>%
+  group_by(month, year) %>%
+  summarise(average = round(mean(sumagoles), 2))
+
+# NOTA: A continuación se realizará un pequeño análisis para remover un dato de promedio
+# especificamente el del mes de junio del año 2013, pues complica el ordenamiento en la
+# función de creación de serie de tiempo.
+
+# La siguiente función cuenta los partidos por mes.
+partidos_por_mes <- data_match %>%
+  mutate(fecha = format(date, "%Y-%m")) %>%
+  group_by(fecha) %>%
+  summarise(n_partidos = length(sumagoles))
+
+# Se calcula el promedio de partidos por mes.
+prom <- mean(partidos_por_mes$n_partidos)
+
+# Ahora se calcula la desviación estándar del número de partidos por mes.
+ds <- sd(partidos_por_mes$n_partidos)
+
+# Después, se calcula un intervalo de partidos jugados por mes considerando
+# dos desviaciones de estándar alrededor del promedio.
+(intervalo <- prom + c(-2,2)*ds)
+
+(n_part_jun<-partidos_por_mes$n_partidos[partidos_por_mes$fecha=='2013-06'])
+#Como el numero de partidos de junio no entra en el intervalo de 2
+#desviaciones estandar de la media consideramos no incluirla, para asi
+#poder trabajar más facilmente con la serie de tiempo
+
+# 3. Crea la serie de tiempo del promedio por mes de la suma de goles hasta diciembre de 2019.
+
+# Se ordenan los datos por año.
+promedio_por_mes <- promedio_por_mes[order(promedio_por_mes$year),]
+
+# Se elimina el dato de promedio del mes de junio de 2013, esto es, el dato 31 del data frame promedio_por_mes.
+promedio_por_mes <- promedio_por_mes[-31,]
+
+# Se crea la serie de datos, dentro de las fechas indicadas. Además, se realiza la división
+# cada 10 datos, que es la duración de cada temporada, agosto - mayo.
+(sumagoles_mes <- ts(promedio_por_mes$average, start = c(2010,8), end = c(2019,5), fr = 10))
+# Se tomó la decisión de considerar sólo los datos de agosto 2010 a mayo 2019 porque estos representan
+# correctamente temporadas de juego completas.
+
+# NOTA: Además es la mejor decisión, pues el tratamiento de datos con las herramientas provistas en este módulo,
+# necesita de valores espaciados de manera equitativa a través del tiempo.
+
+# 4. Se grafica la serie de tiempo.
+plot(sumagoles_mes, main = "Serie de tiempo", xlab = "Tiempo (año)", ylab = "Promedio de suma de goles")
+# Se lleva a cabo una descomposición de la serie de tiempo mediante el uso del método aditivo.
+plots_ts <- decompose(sumagoles_mes)
+plot(plots_ts)
+
+# Alternativamente, sí es necesario (según el objetivo que tenga el cliente acerca de la visualización de esos datos,
+# hasta diciembre de 2019) entonces se puede sólo graficar los datos como tal para ver su comportamiento, sin
+# preocuparse por su distribución en el tiempo, ya que el contexto de temporada está claro.
+sumagoles_mes_alternativa <- zoo(promedio_por_mes[1:95,])
+plot.ts(sumagoles_mes_alternativa$average, main = "Serie de tiempo", xlab = "Tiempo (número de mes jugado)", ylab = "Promedio de suma de goles")
+
+
+
+# POSTWORK 7
+
+# Utilizando el manejador de BDD Mongodb Compass (previamente instalado), deberás de realizar las siguientes acciones:
+
+library("mongolite")
+
+# Alojar el fichero match.data.csv en una base de datos llamada match_games, nombrando al collection como match
+data_match_games <- read.csv("https://raw.githubusercontent.com/beduExpert/Programacion-R-Santander-2021/main/Sesion-07/Postwork/match.data.csv")
+
+m <- mongo(collection = "match", db = "match_games")
+m$insert(data_match_games)
+
+# Una vez hecho esto, realizar un count para conocer el número de registros que se tiene en la base
+(m$count())
+
+# Si se vuelve a cargar los datos a la base de datos, usar la siguiente instrucción para eliminar la base de datos
+# y volver a crearla
+# m$drop()
+
+# Para visualizar todos los datos se puede ejecutar la siguiente instrucción (formato JSON). 
+# (m$find('{}'))
+
+# Realiza una consulta utilizando la sintaxis de Mongodb en la base de datos, para conocer el número de goles
+# que metió el Real Madrid el 20 de diciembre de 2015 y contra que equipo jugó, ¿perdió ó fue goleada?
+# La consulta requerida se obtiene al usar el formato JSON de la siguiente forma
+(consulta <- m$find('{"date" : "2015-12-20", "home_team" : "Real Madrid"}'))
+
+# Por último, no olvides cerrar la conexión con la BDD
+m$disconnect(gc = TRUE) 
 
